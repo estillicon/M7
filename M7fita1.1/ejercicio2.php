ebb1d54cf112b0b086b3edd6075463c6aff81904
estillicon <151068249+estillicon@users.noreply.github.com>
2024-09-19 19:38:14 +0200

    Add files via upload

---
+++ M7fita1.1/ejercicio2.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
+    <table style="border:1px solid black;">
+
+    <TR>
+        
+
+        <?php
+            $var = 10;
+
+            
+
+            for($i=0; $i<=7; $i++) {
+                $var =chr(65+$i);
+                
+
+                echo "<td style=border:1px solid black;
+                border-collapse: collapse;>$var</td> ";
+
+
+
+            }
+
+    
+    
+        ?>
+
+        
+
+    </TR>
+
+
+
+    <TR>
+
+
+        <?php
+
+            for($i=0; $i<=7; $i++) {
+
+                echo "<td style=border:1px solid black;
+                border-collapse: collapse;>$i</td> ";
+
+            }
+        
+        
+        ?>
+
+
+
+
+
+    </TR>
+
+
+
+    </table>
+
+</body>
+</html>
